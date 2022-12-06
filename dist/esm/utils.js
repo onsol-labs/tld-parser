709bd266f64c9ab7a5d8520cbb3513d56ccb7c69
crypt0miester <crypt0miester@gmail.com>
2022-12-06 15:36:46 +0000

    add dist

---
+++ dist/esm/utils.js
@@ -0,0 +1,98 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import { PublicKey } from '@solana/web3.js';
+import { createHash } from 'crypto';
+import { ANS_PROGRAM_ID, ORIGIN_TLD } from './constants';
+import { NameRecordHeader } from './state';
+/**
+ * retrieves raw name account
+ *
+ * @param hashedName hashed name of the name account
+ * @param nameClass defaults to pubkey::default()
+ * @param parentName defaults to pubkey::default()
+ */
+export function getNameAccountKeyWithBump(hashedName, nameClass, parentName) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const seeds = [
+            hashedName,
+            nameClass ? nameClass.toBuffer() : Buffer.alloc(32),
+            parentName ? parentName.toBuffer() : Buffer.alloc(32),
+        ];
+        return yield PublicKey.findProgramAddress(seeds, ANS_PROGRAM_ID);
+    });
+}
+/**
+ * retrieves owner of the name account
+ *
+ * @param connection sol connection
+ * @param nameAccountKey nameAccount to get owner of.
+ */
+export function getNameOwner(connection, nameAccountKey) {
+    var _a;
+    return __awaiter(this, void 0, void 0, function* () {
+        return (_a = (yield NameRecordHeader.fromAccountAddress(connection, nameAccountKey))) === null || _a === void 0 ? void 0 : _a.owner;
+    });
+}
+/**
+ * computes hashed name
+ *
+ * @param name any string or domain name.
+ */
+export function getHashedName(name) {
+    const input = NameRecordHeader.HASH_PREFIX + name;
+    const buffer = createHash('sha256').update(input, 'utf8').digest();
+    return buffer;
+}
+/**
+ * A constant in tld house.
+ *
+ * get origin name account should always equal to 3mX9b4AZaQehNoQGfckVcmgmA6bkBoFcbLj9RMmMyNcU
+ *
+ * @param originTld
+ */
+export function getOriginNameAccountKey(originTld = ORIGIN_TLD) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const hashed_name = getHashedName(originTld);
+        const [nameAccountKey] = yield getNameAccountKeyWithBump(hashed_name, undefined, undefined);
+        return nameAccountKey;
+    });
+}
+/**
+ * finds list of all name accounts for a particular user.
+ *
+ * @param connection sol connection
+ * @param userAccount user's public key
+ * @param parentAccount nameAccount's parentName
+ */
+export function findOwnedNameAccountsForUser(connection, userAccount, parentAccount) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const filters = [
+            {
+                memcmp: {
+                    offset: 40,
+                    bytes: userAccount.toBase58(),
+                },
+            },
+        ];
+        if (parentAccount) {
+            filters.push({
+                memcmp: {
+                    offset: 8,
+                    bytes: parentAccount.toBase58(),
+                },
+            });
+        }
+        const accounts = yield connection.getProgramAccounts(ANS_PROGRAM_ID, {
+            filters: filters,
+        });
+        return accounts.map((a) => a.pubkey);
+    });
+}
+//# sourceMappingURL=utils.js.map
---
+++ dist/types/state.d.ts
@@ -0,0 +1,50 @@
+/// <reference types="node" />
+import { Connection, PublicKey } from '@solana/web3.js';
+import { Schema } from 'borsh';
+/**
+ * Holds the data for the {@link NameRecordHeader} Account and provides de/serialization
+ * functionality for that data
+ */
+export declare class NameRecordHeader {
+    constructor(obj: {
+        parentName: Uint8Array;
+        owner: Uint8Array;
+        nclass: Uint8Array;
+        expiresAt: Uint8Array;
+    });
+    parentName: PublicKey;
+    owner: PublicKey | undefined;
+    nclass: PublicKey;
+    expiresAt: Date;
+    isValid: boolean;
+    data: Buffer | undefined;
+    static DISCRIMINATOR: number[];
+    static HASH_PREFIX: string;
+    /**
+     * NameRecordHeader Schema across all alt name service accounts
+     */
+    static schema: Schema;
+    /**
+     * Returns the minimum size of a {@link Buffer} holding the serialized data of
+     * {@link NameRecordHeader}
+     */
+    static get byteSize(): number;
+    /**
+     * Retrieves the account info from the provided address and deserializes
+     * the {@link NameRecordHeader} from its data.
+     */
+    static fromAccountAddress(connection: Connection, nameAccountKey: PublicKey): Promise<NameRecordHeader | undefined>;
+    /**
+     * Returns a readable version of {@link NameRecordHeader} properties
+     * and can be used to convert to JSON and/or logging
+     */
+    pretty(): {
+        parentName: string;
+        owner: string;
+        nclass: string;
+        expiresAt: Date;
+        isValid: boolean;
+        data: string;
+    };
+}
+//# sourceMappingURL=state.d.ts.map
