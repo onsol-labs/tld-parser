760cab752d974cce32b9dec9a13d448ffc7d2f45
crypt0miester <crypt0miester@gmail.com>
2022-12-07 14:11:08 +0000

    remove dist folder

--- dist/types/constants.d.ts
+++
@@ -1,4 +0,0 @@
-import { PublicKey } from '@solana/web3.js';
-export declare const ORIGIN_TLD = "ANS";
-export declare const ANS_PROGRAM_ID: PublicKey;
-//# sourceMappingURL=constants.d.ts.map
--- dist/types/index.d.ts
+++
@@ -1,7 +0,0 @@
-export * from './utils';
-export * from './constants';
-export * from './parsers';
-export * from './state';
-export * from './name-record-handler';
-export * from './types/records';
-//# sourceMappingURL=index.d.ts.map
