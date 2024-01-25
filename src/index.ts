922179760ba938271bbab0ff7d2d8f00abb7f641
crypt0miester <crypt0miester@gmail.com>
2024-01-25 06:10:22 +0400

    bump version to 0.6.2 and add NftRecord to index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from './constants';
 export * from './parsers';
 export * from './state/name-record-header';
 export * from './state/main-domain';
+export * from './state/nft-record';
 export * from './name-record-handler';
 export * from './types/records';
